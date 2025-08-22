02e83412678b28cab4a94160019d4a861a084542
Pane Trajkov <trajkovpane@outlook.com>
2025-08-22 20:54:46 +0200

    Added Class02 REST API homework solution and project files

---
+++ Class02.RestApi.Homework/Class02.Homework.WebApiApp/WebApiApp/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiApp
+{
+    public class StaticDb
+    {
+        public static List<string> Username { get; set; } = new List<string>
+        {
+            "bob.bobsky",
+            "john.doe",
+            "alice.aliceson",
+            "charlie.charlieson",
+            "dave.daveson"
+        };
+    }
+}
