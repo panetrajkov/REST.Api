8de80e9b70839499423f29b76a9a42ad4b173bc4
Pane Trajkov <trajkovpane@outlook.com>
2025-08-22 19:42:02 +0200

    Add files via upload
    Added Class02 REST API homework solution and project files

---
+++ Class02.Homework.WebApiApp/WebApiApp/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            try
+            {
+                return Ok(StaticDb.Username);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving users.");
+            }
+        }
+
+        [HttpGet("{username}")]
+        public IActionResult GetSingleUser(string username)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+                    return BadRequest("Username cannot be null or empty.");
+                }
+
+                string lowerCaseUsername = username.ToLower();
+                string foundUser = StaticDb.Username.FirstOrDefault(u => u.ToLower() == lowerCaseUsername);
+
+                if (foundUser != null)
+                {
+                    return Ok(foundUser);
+                }
+
+                return NotFound($"User '{username}' not found.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving user.");
+            }
+        }
+    }
+}
